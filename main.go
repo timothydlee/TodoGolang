4e6c2d45752a8b0dd1cc4b2c2b81eb000f2a543c
Tim Lee <timothy.lee@intlfcstone.com>
2019-04-23 17:48:22 -0400

    Continuing to play around.

--- main.go
+++ main.go
@@ -5,5 +5,16 @@ import (
 )
 
 func main() {
-	fmt.Println("HI")
+	// var myInt int
+	// myInt = 42
+
+	var myFloat32 float32 = 42.
+	fmt.Println(myFloat32)
+
+	myString := "Hello Go"
+	fmt.Println(myString)
+
+	myComplex := complex(2, 3)
+	fmt.Println(myComplex)
+	fmt.Println(real(myComplex))
 }
